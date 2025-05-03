372ec6dfd7687f6cdb3038064360b2a0eeb81bc9
Julfani nurdin nst <julfanninst123@gmail.com>
2025-05-03 19:13:08 +0700

    [UPDATE] - implementasi data API

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import RegisterPage from "../Pages/RegisterPage";
+import LoginPage from "../pages/LoginPage";
 
 function WmbRoutes() {
 	return (
 		<BrowserRouter>
 			<Routes>
+				<Route path="/" element={<h1>Welcome to WMB</h1>} />
+				<Route path="/login" element={<LoginPage />} />
 				<Route path="/register" element={<RegisterPage />} />
+				<Route path="*" element={<h1>404 Not Found</h1>} />
 			</Routes>
 		</BrowserRouter>
 	);
